8c81658b389943a6b92d792bcc7a103cb91e50ae
fakincow <zabaroth@gmail.com>
2022-10-08 03:42:18 +0300

    error handler

--- angular14/src/app/app.component.ts
+++ angular14/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {IProduct} from "./models/product";
 import {products, products as data} from "./data/products";
 import {ProductsService} from "./services/products.service";
+import {Observable, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -10,17 +11,20 @@ import {ProductsService} from "./services/products.service";
 })
 export class AppComponent implements OnInit{
   title = 'ANGULAR 14  2022';
-  products: IProduct[] = [];
+  //products: IProduct[] = [];
   loading = false;
-
+  products$: Observable<IProduct[]>
   constructor(private productsService: ProductsService) {
   }
   ngOnInit(): void {
     this.loading = true;
-    this.productsService.getAll().subscribe(products => {
+    this.products$ = this.productsService.getAll().pipe(
+      tap(()=> this.loading = false)
+    );
+   /* this.productsService.getAll().subscribe(products => {
       this.products = products;
       this.loading = false;
-    })
+    })*/
   }
 
 }
